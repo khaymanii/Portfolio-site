53203c9ac4a066e49b35e5ff89b946b8cf76cb5d
kennethakpo <kennethakpo61@gmail.com>
2023-09-01 11:44:35 +0100

    updated work folder

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -5,7 +5,7 @@ export const WorkData = [
         dateJoining: "May 2023",
         dateEnd: "Present",
         companyLogo:"https://omdena.com/wp-content/uploads/2022/10/Favicon.png",
-        work:"•	Collaborated with 30 to 40 team members to build and integrate trained models. • Served as Lead Github project manager, overseeing version control, issue tracking and collaboration among team members. • Prototyped machine learning applications and quickly determined application viability. •	Implemented artificial intelligence and machine learning algorithms that solve real world problems.",
+        work:"•	Collaborated with 30 to 40 team members to build and integrate trained models, i served as lead github project manager, overseeing version control, issue tracking and collaboration among team members. I prototyped machine learning applications and quickly determined application viability, i also implemented artificial intelligence and machine learning algorithms that solve real world problems.",
     },
     {
         company: "Freelancer" ,
@@ -13,6 +13,6 @@ export const WorkData = [
         dateJoining: "February 2022",
         dateEnd: "Present",
         companyLogo:"https://seeklogo.com/images/F/freelancer-com-logo-2B5CE1A961-seeklogo.com.png",
-        work:"• Collaborated with multidisciplinary teams to design and implement new technology features.  • Worked closely with other team members in such tasks as troubleshooting and debugging. • Developed and implemented personalized client solutions using programming language expertise."
+        work:"• Collaborated with multidisciplinary teams to design and implement new technology features. I worked closely with other team members in such tasks as troubleshooting and debugging. I developed and implemented personalized client solutions using programming language expertise, designed reusable and reliable code for use within distributed cloud environments."
     },
 ]
