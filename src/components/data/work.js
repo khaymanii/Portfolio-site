0cddd974b9f90cb6a5bc57ada81c0d67ac2cfac4
khaymanii <kennethakpo61@gmail.com>
2024-08-06 12:46:48 +0100

    updated portfolio

--- src/components/data/work.js
+++ src/components/data/work.js
@@ -1,10 +1,18 @@
 export const WorkData = [
-    {
-        company: "Tublian" ,
-        designation: "Frontend Engineer",
-        dateJoining: "January 2023",
-        dateEnd: "Present",
-        companyLogo:"https://www.tublian.com/static/media/new-logo.0852124c.svg",
-        work:"• Collaborated with multidisciplinary teams to design and implement new technology features. I worked closely with other team members in such tasks as troubleshooting and debugging. Actively participating in the development of open-source software by submitting code, bug fixes, or new features to different companies e.g Next UI, Biodrop, Microsoft LightGBM. Promoting the open-source project through various channels, participating in conferences or meetups, and encouraging community involvement."
-    },
-]
+  {
+    company: "Jurisprudocs",
+    designation: "Frontend Engineer",
+    dateJoining: "February 2024",
+    dateEnd: "May 2024",
+    companyLogo: "",
+    work: " Created a new client dashboard that improved user engagement by 30%.  Implemented a new frontend-framework e.g., React which decreased page load times by 20%.  Collaborated with multidisciplinary teams to design and implement 5 new technology features resulting in a 10% improvement in user satisfaction.  Led the redesign of the company’s main landing page and minimum viable product, resulting in a 25% increase in traffic and 15% increase in client enquiries.  Built a comprehensive style guide and component library that improved development efficiency by 40%, cutting the average feature development time by two weeks.",
+  },
+  {
+    company: "Tublian",
+    designation: "Frontend Engineer",
+    dateJoining: "January 2023",
+    dateEnd: "December 2023",
+    companyLogo: "",
+    work: " Collaborated with multidisciplinary teams to design and implement 5 new technology features resulting in a 10% improvement in user satisfaction.  Actively participated in the development of open-source software by submitting over 10 – 20 code contribution, including bug fixes and new features to Next UI, Biodrop, Microsoft LightGBM codebase.  Engaged with 40 developers, maintaining open lines of communication, and collaborated with contributors to enhance project codebase which led to 20% increase in code quality and reduced bug by 30%.  Built and maintained a personal open-source project called JSFusion attracting few collaborators and receiving few github stars.",
+  },
+];
